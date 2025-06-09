d04172428f5b3d6e90c9fe2ed1eb6a170b4a2023
Erick Castilho <castilho.erick@aluno.ifsp.edu.br>
2025-06-09 17:16:01 -0300

    Primeiro commit do projeto Netflix-Marilene

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import '../Footer/Footer.css';
+
+
+export default function Footer() {
+    return (
+        <footer>
+            <div className="rodape__desenvolvido">
+                <p>Desenvolvido com React JS por Erick Castilho</p>
+                <p>Copyright © 2025</p>
+                <p>Direitos de imagem para Netflix</p>
+                <p>Dados consumidos do Themoviedb.org</p>
+            </div>
+        </footer>
+    );
+}
